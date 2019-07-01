c1b46de1fdd5b219777a687b159399c06d386f87
Siddarth Kaki <siddarthkaki@utexas.edu>
2019-07-01 09:56:36 -0500

    Moved cost functor to separate header file

---
+++ cost_functor.h
@@ -0,0 +1,76 @@
+#ifndef COST_FUNCTOR_H_
+#define COST_FUNCTOR_H_
+
+#include <Eigen/Core>
+#include <math.h>
+
+#include "ceres/ceres.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Eigen::Vector3d;
+
+// A templated cost functor for automatic differentiation
+// Measurements: yVec
+// Feature Points: rFeaMat
+// Camera Location: rCamVec
+// States: posVec, eulVec
+class MeasResidCostFunctor
+{
+    public: MeasResidCostFunctor(VectorXd yVec, MatrixXd rFeaMat, Vector3d rCamVec): yVec_(yVec), rFeaMat_(rFeaMat), rCamVec_(rCamVec_) {}
+
+    template <typename T>
+    bool operator()(const T* const posArr,
+                    const T* const eulArr,
+                    T* residuals) const
+    {
+
+        Eigen::Matrix<T, 3, 1> posVec;
+        posVec(0) = posArr[0];
+        posVec(1) = posArr[1];
+        posVec(2) = posArr[2];
+
+        int numPts = rFeaMat_.rows();
+        int numMeas = yVec_.rows();
+
+        // x[0] = static_cast<T>(0) <- way to keep in Jet type
+
+        for (int idx = 0; idx < numPts; idx++)
+        {
+            Vector3d rFeaVeci = rFeaMat_.block(idx,0,1,3).transpose();
+            
+            //residuals[2*idx+0] = measResidVec(2*idx+0);
+            //residuals[2*idx+1] = measResidVec(2*idx+1);
+
+            residuals[2*idx+0] = yVec_(2*idx+0) - static_cast<T>(atan2(posVec(0) - rCamVec_(0) + rFeaVeci(0)*(cos(eulArr[2])*cos(eulArr[1]) - sin(eulArr[2])*sin(eulArr[1])*sin(eulArr[0])) + rFeaVeci(1)*(cos(eulArr[1])*sin(eulArr[2]) + cos(eulArr[2])*sin(eulArr[1])*sin(eulArr[0])) - rFeaVeci(2)*sin(eulArr[1])*cos(eulArr[0]), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(eulArr[2])*sin(eulArr[1]) + cos(eulArr[1])*sin(eulArr[2])*sin(eulArr[0])) + rFeaVeci(1)*(sin(eulArr[2])*sin(eulArr[1]) - cos(eulArr[2])*cos(eulArr[1])*sin(eulArr[0])) + rFeaVeci(2)*cos(eulArr[1])*cos(eulArr[0])));
+            residuals[2*idx+1] = yVec_(2*idx+1) - static_cast<T>(atan2(posVec(1) - rCamVec_(1) + rFeaVeci(2)*sin(eulArr[0]) + rFeaVeci(1)*cos(eulArr[2])*cos(eulArr[0]) - rFeaVeci(0)*sin(eulArr[2])*cos(eulArr[0]), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(eulArr[2])*sin(eulArr[1]) + cos(eulArr[1])*sin(eulArr[2])*sin(eulArr[0])) + rFeaVeci(1)*(sin(eulArr[2])*sin(eulArr[1]) - cos(eulArr[2])*cos(eulArr[1])*sin(eulArr[0])) + rFeaVeci(2)*cos(eulArr[1])*cos(eulArr[0])));
+            
+//            yHatVec(2*idx+0) = atan2(posVec(0) - rCamVec_(0) + rFeaVeci(0)*(cos(psi)*cos(theta) - sin(psi)*sin(theta)*sin(phi)) + rFeaVeci(1)*(cos(theta)*sin(psi) + cos(psi)*sin(theta)*sin(phi)) - rFeaVeci(2)*sin(theta)*cos(phi), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(psi)*sin(theta) + cos(theta)*sin(psi)*sin(phi)) + rFeaVeci(1)*(sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi)) + rFeaVeci(2)*cos(theta)*cos(phi));
+//            yHatVec(2*idx+1) = atan2(posVec(1) - rCamVec_(1) + rFeaVeci(2)*sin(phi) + rFeaVeci(1)*cos(psi)*cos(phi) - rFeaVeci(0)*sin(psi)*cos(phi), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(psi)*sin(theta) + cos(theta)*sin(psi)*sin(phi)) + rFeaVeci(1)*(sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi)) + rFeaVeci(2)*cos(theta)*cos(phi));
+        }
+
+        return true;
+    }
+
+    private:
+        VectorXd yVec_;
+        MatrixXd rFeaMat_;
+        Vector3d rCamVec_;
+
+    public:
+    // Factory to hide the construction of the CostFunction object from
+    // the client code.
+    /*
+    static ceres::CostFunction* Create( const VectorXd yVec,
+                                        const MatrixXd rFeaMat,
+                                        const Vector3d rCamVec_ )
+    {
+        return (new ceres::AutoDiffCostFunction<MeasResidCostFunctor, 8, 3, 3>(
+            new MeasResidCostFunctor(yVec, rFeaMat, rCamVec)));
+
+    }
+    */
+
+};
+
+#endif // COST_FUNCTOR_H_
--- pose_estimator.cc
+++ pose_estimator.cc
@@ -4,83 +4,17 @@
 #include "ceres/ceres.h"
 #include "glog/logging.h"
 
-using ceres::AutoDiffCostFunction;
-using ceres::CostFunction;
-using ceres::Problem;
-using ceres::Solver;
-using ceres::Solve;
+#include "cost_functor.h"
 
 using Eigen::Vector3d;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using Eigen::Vector3d;
 
-// A templated cost functor for automatic differentiation
-// Measurements: yVec
-// Feature Points: rFeaMat
-// Camera Location: rCamVec
-// States: posVec, eulVec
-class MeasResidCostFunctor
-{
-    public: MeasResidCostFunctor(VectorXd yVec, MatrixXd rFeaMat, Vector3d rCamVec): yVec_(yVec), rFeaMat_(rFeaMat), rCamVec_(rCamVec_) {}
-
-    template <typename T>
-    bool operator()(const T* const posArr,
-                    const T* const eulArr,
-                    T* residuals) const
-    {
-
-        Eigen::Matrix<T, 3, 1> posVec;
-        posVec(0) = posArr[0];
-        posVec(1) = posArr[1];
-        posVec(2) = posArr[2];
-
-        int numPts = 4;//rFeaMat_.rows();
-        int numMeas = yVec_.rows();
-
-        // x[0] = static_cast<T>(0) <- way to keep in Jet type
-
-        for (int idx = 0; idx < numPts; idx++)
-        {
-            Vector3d rFeaVeci = rFeaMat_.block(idx,0,1,3).transpose();
-            
-            //residuals[2*idx+0] = measResidVec(2*idx+0);
-            //residuals[2*idx+1] = measResidVec(2*idx+1);
-
-            residuals[2*idx+0] = yVec_(2*idx+0) - static_cast<T>(atan2(posVec(0) - rCamVec_(0) + rFeaVeci(0)*(cos(eulArr[2])*cos(eulArr[1]) - sin(eulArr[2])*sin(eulArr[1])*sin(eulArr[0])) + rFeaVeci(1)*(cos(eulArr[1])*sin(eulArr[2]) + cos(eulArr[2])*sin(eulArr[1])*sin(eulArr[0])) - rFeaVeci(2)*sin(eulArr[1])*cos(eulArr[0]), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(eulArr[2])*sin(eulArr[1]) + cos(eulArr[1])*sin(eulArr[2])*sin(eulArr[0])) + rFeaVeci(1)*(sin(eulArr[2])*sin(eulArr[1]) - cos(eulArr[2])*cos(eulArr[1])*sin(eulArr[0])) + rFeaVeci(2)*cos(eulArr[1])*cos(eulArr[0])));
-            residuals[2*idx+1] = yVec_(2*idx+1) - static_cast<T>(atan2(posVec(1) - rCamVec_(1) + rFeaVeci(2)*sin(eulArr[0]) + rFeaVeci(1)*cos(eulArr[2])*cos(eulArr[0]) - rFeaVeci(0)*sin(eulArr[2])*cos(eulArr[0]), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(eulArr[2])*sin(eulArr[1]) + cos(eulArr[1])*sin(eulArr[2])*sin(eulArr[0])) + rFeaVeci(1)*(sin(eulArr[2])*sin(eulArr[1]) - cos(eulArr[2])*cos(eulArr[1])*sin(eulArr[0])) + rFeaVeci(2)*cos(eulArr[1])*cos(eulArr[0])));
-            
-//            yHatVec(2*idx+0) = atan2(posVec(0) - rCamVec_(0) + rFeaVeci(0)*(cos(psi)*cos(theta) - sin(psi)*sin(theta)*sin(phi)) + rFeaVeci(1)*(cos(theta)*sin(psi) + cos(psi)*sin(theta)*sin(phi)) - rFeaVeci(2)*sin(theta)*cos(phi), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(psi)*sin(theta) + cos(theta)*sin(psi)*sin(phi)) + rFeaVeci(1)*(sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi)) + rFeaVeci(2)*cos(theta)*cos(phi));
-//            yHatVec(2*idx+1) = atan2(posVec(1) - rCamVec_(1) + rFeaVeci(2)*sin(phi) + rFeaVeci(1)*cos(psi)*cos(phi) - rFeaVeci(0)*sin(psi)*cos(phi), posVec(2) - rCamVec_(2) + rFeaVeci(0)*(cos(psi)*sin(theta) + cos(theta)*sin(psi)*sin(phi)) + rFeaVeci(1)*(sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi)) + rFeaVeci(2)*cos(theta)*cos(phi));
-        }
-
-        return true;
-    }
-
-    private:
-        VectorXd yVec_;
-        MatrixXd rFeaMat_;
-        Vector3d rCamVec_;
-
-    public:
-    // Factory to hide the construction of the CostFunction object from
-    // the client code.
-    static ceres::CostFunction* Create( const VectorXd yVec,
-                                        const MatrixXd rFeaMat,
-                                        const Vector3d rCamVec_ )
-    {
-        return (new ceres::AutoDiffCostFunction<MeasResidCostFunctor, 8, 3, 3>(
-            new MeasResidCostFunctor(yVec, rFeaMat, rCamVec_)));
-
-    }
-
-};
-
-
 
 int main(int argc, char** argv)
 {
-    google::InitGoogleLogging(argv[0]);
+    //google::InitGoogleLogging(argv[0]);
 
     // The variables to solve for with its initial value.
     // The veriables will be mutated in place by the solver.
@@ -110,26 +44,26 @@ int main(int argc, char** argv)
     Vector3d rCamVec;
     rCamVec << 0.0, 0.0, 0.0;
 
-    int numMeas = rFeaMat.rows();
+    int numPts = rFeaMat.rows();
 
     // Build the problem.
-    Problem problem;
+    ceres::Problem problem;
 
     // Set up the only cost function (also known as residual). This uses
     // auto-differentiation to obtain the derivative (jacobian).
     //ceres::CostFunction* cost_function =
     //    MeasResidCostFunctor::Create(yVec, rFeaMat, rCamVec);
-    CostFunction* cost_function = new AutoDiffCostFunction<MeasResidCostFunctor, 8, 3, 3>(
-            new MeasResidCostFunctor(yVec, rFeaMat, rCamVec));
+    ceres::CostFunction* cost_function = new ceres::AutoDiffCostFunction<MeasResidCostFunctor, ceres::DYNAMIC, 3, 3>(
+            new MeasResidCostFunctor(yVec, rFeaMat, rCamVec), numPts*2);
     
     problem.AddResidualBlock(cost_function, NULL, posArr, eulArr);
 
-    // Run the solver!
-    Solver::Options options;
+    // Run the solver
+    ceres::Solver::Options options;
     options.minimizer_type = ceres::TRUST_REGION;
     options.trust_region_strategy_type = ceres::LEVENBERG_MARQUARDT;
     options.minimizer_progress_to_stdout = true;
-    Solver::Summary summary;
+    ceres::Solver::Summary summary;
     Solve(options, &problem, &summary);
 
     std::cout << summary.BriefReport() << "\n";
