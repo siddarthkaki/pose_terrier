371c3fbd7e0df02a8536f17ba10b06c2231b5281
Siddarth Kaki <siddarthkaki@utexas.edu>
2019-08-05 12:38:42 -0500

    Updated MATLAB plotting script to display pos_score over time

--- scripts/plot_poses.m
+++ scripts/plot_poses.m
@@ -19,18 +19,17 @@ tVec = 0:dt:(num_poses-1)*dt;
 
 %% compute position score
 
-possScoreVec = 100*ones(num_poses,1);
+posScoreVec         = 100*ones(num_poses,1);
+posScoreVecFiltered = 100*ones(num_poses,1);
 
-% for idx = 1:num_poses,
-% 
-%     quat    = angle2quat(     truePosesMat(idx,4),     truePosesMat(idx,5),     truePosesMat(idx,6) );
-%     quatHat = angle2quat( filteredPosesMat(idx,4), filteredPosesMat(idx,5), filteredPosesMat(idx,6) );
-%     
-%     dquat = quatmultiply( quatnormalize(quat), quatconj(quatnormalize(quatHat)) );
-%     %dqVec = (quat.normalized())*(quatHat.normalized().conjugate());
-% 
-%     attScoreVec(idx) = 2*acosd( abs( dquat(1) ) ); % deg
-% end
+for idx = 1:num_poses,
+
+    posErr         = truePosesMat(idx,1:3) - solvedPosesMat(idx,1:3);
+    posErrFiltered = truePosesMat(idx,1:3) - filteredPosesMat(idx,1:3);
+
+    posScoreVec(idx)         = norm(posErr)/norm(truePosesMat(idx,1:3));
+    posScoreVecFiltered(idx) = norm(posErrFiltered)/norm(truePosesMat(idx,1:3));
+end
 
 %% compute attitude score
 
@@ -54,7 +53,7 @@ end
 
 %% plotting position
 figure(1)
-subplot(3,1,1)
+subplot(4,1,1)
 plot(tVec, truePosesMat(:,1))
 title('Position')
 hold on
@@ -65,7 +64,7 @@ legend('true','NLS','KF','Location','Northwest')
 xlabel('time [s]')
 ylabel('x [m]')
 
-subplot(3,1,2)
+subplot(4,1,2)
 plot(tVec, truePosesMat(:,2))
 hold on
 plot(tVec, solvedPosesMat(:,2))
@@ -75,7 +74,7 @@ legend('true','NLS','KF','Location','Southwest')
 xlabel('time [s]')
 ylabel('y [m]')
 
-subplot(3,1,3)
+subplot(4,1,3)
 plot(tVec, truePosesMat(:,3))
 hold on
 plot(tVec, solvedPosesMat(:,3))
@@ -85,6 +84,15 @@ legend('true','NLS','KF','Location','Northwest')
 xlabel('time [s]')
 ylabel('z [m]')
 %ylim([0,50])
+
+subplot(4,1,4)
+plot(tVec, posScoreVec,'color',[0.8500, 0.3250, 0.0980])
+hold on
+plot(tVec, posScoreVecFiltered,'color',[0.4940, 0.1840, 0.5560],'LineWidth',2)
+grid on
+legend('NLS','KF')
+xlabel('time [s]')
+ylabel('pos\_score [ ]')
 
 %% plotting attitude
 figure(2)
