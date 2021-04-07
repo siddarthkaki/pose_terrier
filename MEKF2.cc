3b3a4300bc8d1d233ea5a0134a8bb7eb0ce38719
Siddarth Kaki <siddarthkaki@utexas.edu>
2021-04-07 14:11:44 -0500

    Added integrated pos + att MEKF2 class

---
+++ MEKF2.cc
@@ -0,0 +1,239 @@
+/* Copyright (c) 2021 Siddarth Kaki
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ 
+#include "MEKF2.h"
+
+namespace MEKF2 {
+
+    MEKF2::MEKF2(const double &dt)
+    {
+        dt_ = dt;
+        processed_measurement_ = false;
+    }
+
+    void MEKF2::Init(const double &process_noise_std, const double &measurement_noise_std, const double &dt)
+    {
+        num_att_states_ = 9; // delta_gibbs(3), omega(3), alpha(3)
+        num_pos_states_ = 9; // pos(3), posDot(3), posDotDot(3)
+        num_states_ = num_att_states_ + num_pos_states_;
+        num_att_measurements_ = 3; // delta_gibbs(3)
+        num_pos_measurements_ = 3; // pos(3)
+        num_measurements_ = num_pos_measurements_ + num_att_measurements_;
+        dt_ = dt;
+        tau_ = 1.0; // TODO PASS AS ARGUMENT
+        
+        // TODO: spilt process & measurement noise std for pos and att
+
+        Q_ = MatrixXd::Identity(num_states_, num_states_)*pow(process_noise_std,2); // process_noise_covariance
+        double qpsd = 1e5;
+        double pss = qpsd*tau_/2.0;
+        Q_(6,6) *= ( 1.0 - exp(-2.0*dt_/tau_) )*pss;
+        Q_(7,7) *= ( 1.0 - exp(-2.0*dt_/tau_) )*pss;
+        Q_(8,8) *= ( 1.0 - exp(-2.0*dt_/tau_) )*pss;
+        MatrixXd Q_pos_ = MatrixXd::Identity(num_pos_states_, num_pos_states_); // position_process_noise_covariance
+        Q_pos_(0,0) = 0.25*pow(dt,4);
+        Q_pos_(1,1) = 0.25*pow(dt,4);
+        Q_pos_(2,2) = 0.25*pow(dt,4);
+        Q_pos_(3,3) = pow(dt,2);
+        Q_pos_(4,4) = pow(dt,2);
+        Q_pos_(5,5) = pow(dt,2);
+        Q_pos_(6,6) = 1.0;
+        Q_pos_(7,7) = 1.0;
+        Q_pos_(8,8) = 1.0;
+        Q_pos_(0,3) = 0.5*pow(dt,3);
+        Q_pos_(1,4) = 0.5*pow(dt,3);
+        Q_pos_(2,5) = 0.5*pow(dt,3);
+        Q_pos_(3,6) = dt;
+        Q_pos_(4,7) = dt;
+        Q_pos_(5,8) = dt;
+        Q_pos_(3,0) = 0.5*pow(dt,3);
+        Q_pos_(4,1) = 0.5*pow(dt,3);
+        Q_pos_(5,2) = 0.5*pow(dt,3);
+        Q_pos_(6,3) = dt;
+        Q_pos_(7,4) = dt;
+        Q_pos_(8,5) = dt;
+        Q_pos_(0,6) =0.5*pow(dt,2);
+        Q_pos_(1,7) =0.5*pow(dt,2);
+        Q_pos_(2,8) =0.5*pow(dt,2);
+        Q_pos_(6,0) =0.5*pow(dt,2);
+        Q_pos_(7,1) =0.5*pow(dt,2);
+        Q_pos_(8,2) =0.5*pow(dt,2);
+        Q_pos_ = Q_pos_*pow(process_noise_std,2);
+        Q_.bottomRightCorner(num_pos_states_, num_pos_states_) = Q_pos_;
+        
+        R_ = MatrixXd::Identity(num_measurements_, num_measurements_)*pow(measurement_noise_std,2); // measurement_noise_covariance
+
+        A_ = MatrixXd::Identity(4, 4); // quaternion_propagation
+
+        F_ = MatrixXd::Identity(num_states_, num_states_); // convariance_dynamics_propagation
+        F_.block(3, 3, 3, 3) = I33;
+        F_.block(3, 6, 3, 3) = I33 * dt_;
+        F_.block(6, 6, 3, 3) = I33 * exp(-dt_ / tau_);
+        F_pos_ = MatrixXd::Identity(num_pos_states_, num_pos_states_); // position_dynamics_propagation
+        F_pos_(0,3) = dt_;
+        F_pos_(1,4) = dt_;
+        F_pos_(2,5) = dt_;
+        F_pos_(3,6) = dt_;
+        F_pos_(4,7) = dt_;
+        F_pos_(5,8) = dt_;
+        F_pos_(0,6) = 0.5*pow(dt_,2);
+        F_pos_(1,7) = 0.5*pow(dt_,2);
+        F_pos_(2,8) = 0.5*pow(dt_,2);
+        F_.bottomRightCorner(num_pos_states_, num_pos_states_) = F_pos_;
+
+        H_ = MatrixXd::Zero(num_measurements_, num_states_); // measurement_model
+        H_.block(0, 0, num_att_measurements_, num_att_measurements_) = Matrix3d::Identity();
+        H_.block(num_att_measurements_, num_att_states_, num_pos_measurements_, num_pos_measurements_) = Matrix3d::Identity();
+
+         pos_est_ = Vector3d::Zero();
+        quat_est_ = Quaterniond::Identity();
+        delta_gibbs_est_ = Vector3d::Zero();
+        state_est_ = VectorXd::Zero(num_states_);
+
+        covar_est_ = MatrixXd::Zero(num_states_, num_states_);
+
+        processed_measurement_ = false;
+    }
+
+    void MEKF2::SetInitialStateAndCovar(const Quaterniond &quat0, const Vector3d &omega0, const Vector3d &alpha0, const VectorXd &x0, const MatrixXd &covar0)
+    {
+         quat_est_ =  quat0;
+        state_est_.segment(3, 3) = omega0;
+        state_est_.segment(6, 3) = alpha0;
+        state_est_.segment(num_att_states_, num_pos_states_) = x0;
+        covar_est_ = covar0;
+    }
+
+    // Prediction step
+    void MEKF2::Predict()
+    {
+        Vector3d omega_est_ = state_est_.segment(3, 3);
+
+        double omega_norm = omega_est_.norm();
+        Vector3d omega_hat = omega_est_ / omega_norm;
+
+        MatrixXd omega_hat_44_equivalent = MatrixXd::Zero(4, 4);
+        omega_hat_44_equivalent.block(0, 1, 1, 3) = -omega_hat.transpose();
+        omega_hat_44_equivalent.block(1, 0, 3, 1) =  omega_hat;
+        omega_hat_44_equivalent.block(1, 1, 3, 3) = -CppRot::CrossProductEquivalent(omega_hat);
+
+        double phi = 0.5 * omega_norm * dt_;
+
+        A_ = cos(phi) * I44 + sin(phi) * omega_hat_44_equivalent;
+        F_.block(0, 0, 3, 3) = (-CppRot::CrossProductEquivalent(omega_est_) * dt_).exp();
+
+        // propagate quaternion
+        quat_est_ = Utilities::Vec4ToQuat( A_ * Utilities::QuatToVec4(quat_est_) );
+
+        // propagate rest of the state
+        state_est_.tail(num_states_-3) = F_.bottomRightCorner(num_states_-3, num_states_-3)*state_est_.tail(num_states_-3);
+        pos_est_ = state_est_.segment(num_att_states_, 3);
+
+        //std::cout << pos_est_.transpose() << std::endl << std::endl;
+        
+        // propagate covariance
+        covar_est_ = F_ * covar_est_ * F_.transpose() + Q_;
+
+    }
+
+    // Update step
+    void MEKF2::Update(const VectorXd &measurement)
+    {
+        // Kalman gain
+        MatrixXd K = covar_est_ * H_.transpose() * ((H_ * covar_est_ * H_.transpose() + R_).inverse());
+        
+        Matrix3d pos_covar_est = covar_est_.block(num_att_states_, num_att_states_, 3, 3);
+        Matrix3d R_pos = R_.bottomRightCorner(3, 3);
+        Matrix3d K_pos = pos_covar_est * I33.transpose() * ((I33 * pos_covar_est * I33.transpose() + R_pos).inverse());
+
+
+        // delta Gibbs update
+        Quaterniond quat_meas = Utilities::Vec4ToQuat(measurement.head(4));
+        Quaterniond delta_quat = CppRot::QuatMult_S(quat_meas, quat_est_.inverse());
+        Vector3d meas_innovation = 2.0 * delta_quat.vec() / delta_quat.w();
+        delta_gibbs_est_ = K.block(0, 0, 3, 3)*meas_innovation;
+
+        // position update
+        pos_est_ = pos_est_ + K_pos*( measurement.tail(3) - I33*pos_est_ );
+
+        //std::cout << pos_est_.transpose() << std::endl << std::endl;
+
+        state_est_.segment(num_att_states_, 3) = pos_est_;
+        
+        // Joseph update (general)
+        MatrixXd I = MatrixXd::Identity(num_states_, num_states_);
+        covar_est_ = (I - K * H_) * covar_est_ * ((I - K * H_).transpose()) + K * R_ * (K.transpose());
+    }
+
+    // Reset step
+    void MEKF2::Reset()
+    {
+        Quaterniond delta_quat = Quaterniond::Identity();
+        delta_quat.w() = 1.0;
+        delta_quat.vec() = 0.5*delta_gibbs_est_;
+        Quaterniond quat_star = CppRot::QuatMult_S(delta_quat, quat_est_).normalized();
+        
+        
+        // NOTE: heuristic method to ignore 180 deg pose ambiguities
+        Quaterniond dq = CppRot::QuatMult_S(quat_est_, quat_star.inverse());
+        double dangle = 2.0*acos( abs( dq.w() ) );
+        if (dangle < 30.0*Utilities::DEG2RAD)
+        {
+            quat_est_ = quat_star;
+        }
+        
+        quat_est_ = quat_star;
+
+        delta_gibbs_est_ = Vector3d::Zero();        
+       
+        processed_measurement_ = true;
+    }
+
+    void MEKF2::StoreAndClean()
+    {
+        /*
+        if(processed_measurement_)
+        {
+            last_state_estimate = statek1k1_;
+            last_covar_estimate = covark1k1_;
+            //states.push_back(err_statek1k1_);
+            //covars.push_back(covark1k1_);
+
+            statekk_ = statek1k1_;
+            covarkk_ = covark1k1_;
+        }
+        else
+        {
+            last_state_estimate = statek1k_;
+            last_covar_estimate = covark1k_;
+            //states.push_back(err_statek1k_);
+            //covars.push_back(covark1k_);
+
+            statekk_ = statek1k_;
+            covarkk_ = covark1k_;
+        }
+        */
+        
+        processed_measurement_ = false;
+
+        /*
+        statek1k_  = VectorXd::Zero(num_states_);
+        statek1k1_ = VectorXd::Zero(num_states_);
+
+        covark1k_  = MatrixXd::Zero(num_states_covar_, num_states_covar_);
+        covark1k1_ = MatrixXd::Zero(num_states_covar_, num_states_covar_);
+        */
+    }
+
+    void MEKF2::PrintModelMatrices()
+    {
+        std::cout << "A:\t" << std::endl << A_ << std::endl << std::endl;
+        std::cout << "F:\t" << std::endl << F_ << std::endl << std::endl;
+        std::cout << "Q:\t" << std::endl << Q_ << std::endl << std::endl;
+        std::cout << "H:\t" << std::endl << H_ << std::endl << std::endl;
+        std::cout << "R:\t" << std::endl << R_ << std::endl << std::endl;
+    }
+
+} // end namespace
---
+++ MEKF2.h
@@ -0,0 +1,88 @@
+/* Copyright (c) 2021 Siddarth Kaki
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef MEKF2_H_
+#define MEKF2_H_
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/MatrixFunctions>
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+#include "Utilities.h"
+
+#include "third_party/CppRot/cpprot.h"
+
+using Eigen::Vector3d;
+using Eigen::VectorXd;
+using Eigen::Matrix3d;
+using Eigen::MatrixXd;
+using Eigen::Quaterniond;
+using Eigen::AngleAxisd;
+
+namespace MEKF2 {
+
+class MEKF2
+{
+    private:
+        bool processed_measurement_;
+        Vector3d delta_gibbs_est_;
+        VectorXd state_est_;
+
+        Matrix3d I33 = Matrix3d::Identity();
+        MatrixXd I44 = MatrixXd::Identity(4, 4);
+
+    public:
+        
+        double dt_;
+        double tau_;
+
+        unsigned int num_att_states_;
+        unsigned int num_pos_states_;
+        unsigned int num_states_;
+
+        unsigned int num_att_measurements_;
+        unsigned int num_pos_measurements_;
+        unsigned int num_measurements_;
+
+        MatrixXd Q_; // process_noise_covariance
+        MatrixXd R_; // measurement_noise_covariance
+        MatrixXd F_pos_; // position_covariance_propagation_dynamics_model
+        MatrixXd F_; // covariance_propagation_dynamics_model
+        MatrixXd A_; // quaternion_propagation_dynamics_model
+        MatrixXd H_; // measurement_model
+
+        //Vector3d omega_est_;
+        //Vector3d alpha_est_;
+        //VectorXd x_est_;
+
+        Vector3d pos_est_;
+        Quaterniond quat_est_;
+        MatrixXd covar_est_;
+
+        //MatrixXd covarkk_;
+        //MatrixXd covark1k_;
+        //MatrixXd covark1k1_;
+
+        //std::vector<VectorXd> states;
+        //std::vector<MatrixXd> covars;
+
+        MEKF2(const double &dt);
+        MEKF2(const unsigned int &num_states, const unsigned int &num_measurements, const double &dt);
+        void Init(const double &process_noise_std, const double &measurement_noise_std, const double &dt);
+        void SetInitialStateAndCovar(const Quaterniond &quat0, const Vector3d &omega0, const Vector3d &alpha0, const VectorXd &x0, const MatrixXd &covar0);
+        void Predict();
+        void Update(const VectorXd &measurement);
+        void Reset();
+        void StoreAndClean();
+
+        void PrintModelMatrices();
+};
+
+} // end namespace
+
+#endif // MEKF2_H_
